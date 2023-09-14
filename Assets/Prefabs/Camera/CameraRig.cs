d1cad2f222f62a7afc3d7def2df54cd86b8c633e
MonsterMegalodon <megalodonthemonster@outlook.com>
2023-09-14 15:02:38 -0500

    Class2

---
+++ Assets/Prefabs/Camera/CameraRig.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteAlways]
+
+public class CameraRig : MonoBehaviour
+{
+    [SerializeField] Transform followTransform;
+    [SerializeField] float armLength;
+    [SerializeField] Transform cameraTrans;
+    [SerializeField] Transform cameraArm;
+    [SerializeField] float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void AddYawInput(float amt)
+    {
+        transform.Rotate(Vector3.up, amt * Time.deltaTime * turnSpeed);
+    }
+
+    private void LateUpdate()
+    {
+        cameraTrans.position = cameraArm.position - cameraTrans.forward * armLength;
+        transform.position = followTransform.position;
+    }
+}
--- Assets/Prefabs/Player/PlayerCharacter.cs
+++ Assets/Prefabs/Player/PlayerCharacter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
@@ -8,23 +9,30 @@ public class PlayerCharacter : MonoBehaviour
     [SerializeField] private Joystick moveStick;
     [SerializeField] private Joystick aimStick;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] CameraRig cameraRig;
     CharacterController characterController;
+    Vector2 aimInput;
+    Vector2 moveInput;
+
+    Camera viewCamera;
+
     private void Awake()
     {
         moveStick.onInputValueChanged += MoveInputUpdated;
         aimStick.onInputValueChanged += AimInputUpdated;
         //initializing values
         characterController = GetComponent<CharacterController>();
+        viewCamera = Camera.main;
     }
 
     private void AimInputUpdated(Vector2 inputVal)
     {
-
+        aimInput = inputVal;
     }
 
     private void MoveInputUpdated(Vector2 inputVal)
     {
-        characterController.Move(new Vector3(inputVal.x, 0f, inputVal.y) * Time.deltaTime * moveSpeed);
+        moveInput = inputVal;
     }
 
     // Start is called before the first frame update
@@ -36,6 +44,30 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ProccessMoveInput();
+    }
+
+    private void LateUpdate()
+    {
+        UpdateCamera();
+    }
+
+    private void UpdateCamera()
+    {
+        cameraRig.AddYawInput(moveInput.x);
+    }
+
+    void ProccessMoveInput()
+    {
+        Vector3 rightdir = viewCamera.transform.right;
+        //Vector3 upDir = Vector3.Cross(rightdir, Vector3.up);      //cross is more expensive
+
+        //cheeper way
+        Vector3 upDir = viewCamera.transform.forward;
+        upDir.y = 0;
+        upDir = upDir.normalized;
         
+
+        characterController.Move(new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed * Time.deltaTime);
     }
 }
