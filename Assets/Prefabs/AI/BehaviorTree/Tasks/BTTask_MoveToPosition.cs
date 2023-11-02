f55134a0c1980ba26c8ee3cc8ec4e7c60f825598
Li <jili1@uiwtx.edu>
2023-11-02 14:29:36 -0500

    added move to location task

---
+++ Assets/Prefabs/AI/BehaviorTree/Tasks/BTTask_MoveToPosition.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BTTaks_MoveToPosition : BTNode
+{
+    [SerializeField]
+    string targetKeyName = "nextPatrolPoint";
+
+    [SerializeField]
+    float acceptableDistance = 2.0f;
+
+    NavMeshAgent agent;
+    GameObject owner;
+    Vector3 pos;
+    Blackboard blackboard;
+
+    protected override BTNodeResult Execute()
+    {
+        if (!GetBehaviorTree()) return BTNodeResult.Failure;
+
+        blackboard = GetBehaviorTree().GetBlackBoard();
+        if (blackboard == null) return BTNodeResult.Failure;
+
+        if (!blackboard.GetBlackboarData("owner", out owner))
+            return BTNodeResult.Failure;
+
+        agent = owner.GetComponent<NavMeshAgent>();
+        if (!agent) return BTNodeResult.Failure;
+
+        if (!blackboard.GetBlackboarData(targetKeyName, out pos))
+            return BTNodeResult.Failure;
+
+        agent.stoppingDistance = acceptableDistance;
+        agent.SetDestination(pos);
+        agent.isStopped = false;
+
+        return BTNodeResult.InProgress;
+    }
+
+    protected override BTNodeResult Update()
+    {
+        if (InAcceptableDistance())
+        {
+            return BTNodeResult.Success;
+        }
+
+        return BTNodeResult.InProgress;
+    }
+
+    private bool InAcceptableDistance()
+    {
+        return Vector3.Distance(owner.transform.position, pos) <= acceptableDistance;
+    }
+
+    protected override void End()
+    {
+        agent.isStopped = true;
+        base.End();
+    }
+}
